4d7c703154a7cd6f3cdf96df18f13200ae4acd37
Emily Chow <emilykchow@outlook.com>
2026-03-06 16:01:22 -0500

    feat: createrd todos service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from "./components/header/header.component";
+import { TodoListComponent } from "./components/todo-list/todo-list.component";
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, TodoListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
